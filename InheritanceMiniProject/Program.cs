664a00e6eb05653acd01de0a21cf2ce020702d7f
nonex97 <ngrguric97@gmail.com>
2026-07-03 19:20:52 +0200

    Implemented initial Classes for an inventory.

--- InheritanceMiniProject/Program.cs
+++ InheritanceMiniProject/Program.cs
@@ -14,4 +14,20 @@ namespace InheritanceMiniProject
             Console.ReadLine();
         }
     }
+
+    public class InventoryItemModel
+    {
+        public string ProductName { get; set; }
+
+    }
+
+    public class Vehicle : InventoryItemModel
+    {
+        public decimal DealerFee { get; set; }
+    }
+
+    public class BookModel : InventoryItemModel
+    {
+        public int NumberOfPages { get; set; }
+    }
 }
